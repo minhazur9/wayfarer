a584ad2e6d4e23ec3e106e1559909af1f2bc9f7f
min <minhazur9@gmail.com>
2020-11-02 13:40:21 -0500

    Added models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user_image = models.CharField(max_length=255)
+    current_city = models.CharField(max_length=255)
+
+class City(models.Model):
+    city_name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    state_province = models.CharField(max_length=255)
+    city_image = models.CharField(max_length=255)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    photo = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.ManyToManyField(City)
+
+class Comment(models.Model):
+    content = models.TextField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
