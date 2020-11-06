35107afa875ac7372769706e485b91054080bfdd
Howakyl96 <howakyl@gmail.com>
2020-11-05 21:26:30 -0800

    added about page

--- main_app/views.py
+++ main_app/views.py
@@ -11,6 +11,9 @@ from .models import *
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
 def signup(request):
     error_message = ''
     if request.method == 'POST':
