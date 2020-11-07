895c90171e9c8095ea95b753b997ab5da72d6877
min <minhazur9@gmail.com>
2020-11-06 21:37:13 -0500

    testing

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -126,7 +126,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'main_app/wayfarer_project/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'wayfarer_project/staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
