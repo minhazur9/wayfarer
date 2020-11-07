f3598194a03900e928e3c435b74219c5f282ffe9
min <minhazur9@gmail.com>
2020-11-06 21:25:01 -0500

    f

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_on_heroku
+from .secrets import *
 import os
+import django_on_heroku
 
 
 
@@ -126,12 +127,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_REDIRECT_URL = '/dashboard/'
