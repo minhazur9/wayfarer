64d4b98eefe78abce775b05c94c7f0e569b0cdb9
tylerbertmore <tylercsmith94@gmail.com>
2020-11-26 07:02:30 +0700

    Changes and hides secret key, removes your from share your memories

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5ha=j+o60!q5@n)#lx0hy4jxl_)+ws@r@!vban1+gys*&!ph$r'
+SECRET_KEY = process.env.SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
