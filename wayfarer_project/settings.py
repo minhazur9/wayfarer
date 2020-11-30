a86c980add6ad65ef0a9789e128f3a69f884ba06
tylerbertmore <tylercsmith94@gmail.com>
2020-11-30 07:46:34 +0700

    added secret key warning message

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# If you are running this offline change the key to a random string
 SECRET_KEY = os.environ['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
