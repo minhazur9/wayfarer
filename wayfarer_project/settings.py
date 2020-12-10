d1fd82f5e014621c8beeb031fb0cdc86ffa73164
min <minhazur9@gmail.com>
2020-12-09 21:52:50 -0500

    testing

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5ha=j+o60!q5@n)#lx0hy4jxl_)+ws@r@!vban1+gys*&!ph$r'
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
